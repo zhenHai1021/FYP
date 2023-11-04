1b80481405dfb8bf3bbade7e5d2468c203aa9421
zhen_hai <113818064+zhenHai1021@users.noreply.github.com>
2023-11-05 01:32:57 +0800

    Update MoodMentorFlaskAPI.py

--- MoodMentorFlaskAPI.py
+++ MoodMentorFlaskAPI.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 import cv2
 import numpy as np
 import pickle
@@ -8,73 +8,101 @@ import boto3
 app = Flask(__name__)
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 
-# Initialize S3 client outside the route
-s3 = boto3.client('s3')
+def download_file_from_s3(bucket, key, local_path):
+    try:
+        s3.download_file(bucket, key, local_path)
+        print(f"Downloaded {key} from S3")
+    except Exception as e:
+        print(f"An error occurred while downloading {key} from S3: {str(e)}")
 
-# Specify the S3 bucket name
-bucket_name = 'facial-login-model-bucket'  # Replace with your S3 bucket name
 
-# Download the Haar Cascade XML file from S3
-haarcascade_file_key = 'haarcascade_frontalface_default.xml'
-s3.download_file(bucket_name, haarcascade_file_key, 'haarcascade_frontalface_default.xml')
-
-# Load the Haar Cascade XML file
-faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-# Download the 'names.pkl' file from S3
-names_file_key = 'names.pkl'
-s3.download_file(bucket_name, names_file_key, 'names.pkl')
-
-# Load the 'names.pkl' file
-with open('names.pkl', 'rb') as f:
-    names = pickle.load(f)
-
-# Initialize the recognizer
-recognizer = cv2.face_LBPHFaceRecognizer.create()
-model_file_key = 'trainer/trainer.yml'
-s3.download_file(bucket_name, model_file_key, 'trainer.yml')
-recognizer.read('trainer.yml')
-
-font = cv2.FONT_HERSHEY_SIMPLEX
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST', 'GET'])
 def faceRecognition():
-    # Check if the request contains an image file
-    if 'image' not in request.files:
-        return jsonify({'error': 'No image provided'}), 400
+    try:
+        print("Starting the face recognition process...")
+        # Initialize S3 client
+        s3 = boto3.client('s3')
 
-    image = request.files['image'].read()
-    image_array = np.fromstring(image, np.uint8)
-    img = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+        # Specify the S3 bucket name
+        bucket_name = 'facial-login-model-bucket'  # Replace with your S3 bucket name
 
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.2,
-        minNeighbors=5,
-        minSize=(30, 30),
-    )
+        # Load the Haar Cascade XML file
+        print("Loading the Haar Cascade XML file...")
+        faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-    results = []
+        # Download the 'names.pkl' file from S3
+        names_file_key = 'names.pkl'
+        download_file_from_s3(bucket_name, names_file_key, 'names.pkl')
 
-    for (x, y, w, h) in faces:
-        id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
+         # Load the 'names.pkl' file
+        with open('names.pkl', 'rb') as f:
+            names = pickle.load(f)
 
-        if confidence < 100:
-            if id >= 0 and id < len(names):
-                id = names[id]
-                confidence = round(100 - confidence)
-            else:
-                id = "unknown"
-                confidence = round(100 - confidence)
-        else:
-            id = "unknown"
-            confidence = 0
+        # Load the face recognition model
+        recognizer = cv2.face_LBPHFaceRecognizer.create()
+        model_file_key = 's3://facial-login-model-bucket/trainer/trainer.yml'
+        print(f"Downloading face recognition model from {model_file_key}")
+        s3.download_file(bucket_name, model_file_key, 'trainer.yml')
+        recognizer.read('trainer.yml')
 
-        results.append({'user': id, 'confidence': confidence})
+        font = cv2.FONT_HERSHEY_SIMPLEX
 
-    return jsonify(results)
+        total_cameras = 2
+        current_camera = 0
+        cam = cv2.VideoCapture(0)
+        cam.set(3, 640)
+        cam.set(4, 480)
+        minW = 0.1 * cam.get(3)
+        minH = 0.1 * cam.get(4)
+
+        while True:
+            ret, img = cam.read()
+
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+            faces = faceCascade.detectMultiScale(
+                gray,
+                scaleFactor=1.2,
+                minNeighbors=5,
+                minSize=(int(minW), int(minH)),
+            )
+
+            for (x, y, w, h) in faces:
+                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+                id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
+
+                if confidence < 100:
+                    if id >= 0 and id < len(names):
+                        id = names[id]
+                        confidence = "  {0}%".format(round(100 - confidence))
+                    else:
+                        id = "unknown"
+                        confidence = "  {0}%".format(round(100 - confidence))
+
+                cv2.putText(img, str(id), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
+                cv2.putText(img, str(confidence), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
+
+            cv2.imshow('camera', img)
+
+            k = cv2.waitKey(10) & 0xff
+            if k == 27:
+                break
+            elif k == ord('c') or k == ord('C'):
+                current_camera = (current_camera + 1) % total_cameras
+                cam.release()
+                cam = cv2.VideoCapture(current_camera)
+                cam.set(3, 640)
+                cam.set(4, 480)
+
+        print("\nExiting Recognizer.")
+        cam.release()
+        cv2.destroyAllWindows()
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
