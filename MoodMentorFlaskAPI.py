ccb70b35650f7d48f639b2df021955e3b9174594
zhen_hai <113818064+zhenHai1021@users.noreply.github.com>
2023-11-05 00:54:06 +0800

    Update MoodMentorFlaskAPI.py

--- MoodMentorFlaskAPI.py
+++ MoodMentorFlaskAPI.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 import cv2
 import numpy as np
 import pickle
@@ -10,14 +10,6 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 
 @app.route('/')
 def faceRecognition():
-    # Check if the request contains an image file
-    if 'image' not in request.files:
-        return jsonify({'error': 'No image provided'}), 400
-
-    image = request.files['image'].read()
-    image_array = np.fromstring(image, np.uint8)
-    img = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
-
     # Initialize S3 client
     s3 = boto3.client('s3')
 
@@ -46,34 +38,57 @@ def faceRecognition():
 
     font = cv2.FONT_HERSHEY_SIMPLEX
 
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    total_cameras = 2
+    current_camera = 0
+    cam = cv2.VideoCapture(0)
+    cam.set(3, 640)
+    cam.set(4, 480)
+    minW = 0.1 * cam.get(3)
+    minH = 0.1 * cam.get(4)
 
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.2,
-        minNeighbors=5,
-        minSize=(30, 30),
-    )
+    while True:
+        ret, img = cam.read()
 
-    results = []
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    for (x, y, w, h) in faces:
-        id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.2,
+            minNeighbors=5,
+            minSize=(int(minW), int(minH)),
+        )
 
-        if confidence < 100:
-            if id >= 0 and id < len(names):
-                id = names[id]
-                confidence = round(100 - confidence)
-            else:
-                id = "unknown"
-                confidence = round(100 - confidence)
-        else:
-            id = "unknown"
-            confidence = 0
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-        results.append({'id': id, 'confidence': confidence})
+            id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
 
-    return jsonify(results)
+            if confidence < 100:
+                if id >= 0 and id < len(names):
+                    id = names[id]
+                    confidence = "  {0}%".format(round(100 - confidence))
+                else:
+                    id = "unknown"
+                    confidence = "  {0}%".format(round(100 - confidence))
 
-if __name__ == '__main':
+            cv2.putText(img, str(id), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
+            cv2.putText(img, str(confidence), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
+
+        cv2.imshow('camera', img)
+
+        k = cv2.waitKey(10) & 0xff
+        if k == 27:
+            break
+        elif k == ord('c') or k == ord('C'):
+            current_camera = (current_camera + 1) % total_cameras
+            cam.release()
+            cam = cv2.VideoCapture(current_camera)
+            cam.set(3, 640)
+            cam.set(4, 480)
+
+    print("\nExiting Recognizer.")
+    cam.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
