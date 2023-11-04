ddfcfd2d744b8949d610131d33a69dcf53e4050e
zhen_hai <113818064+zhenHai1021@users.noreply.github.com>
2023-11-04 22:24:39 +0800

    Update MoodMentorFlaskAPI.py

--- MoodMentorFlaskAPI.py
+++ MoodMentorFlaskAPI.py
@@ -1,37 +1,49 @@
 from flask import Flask
-
-#login
 import cv2
 import numpy as np
-from PIL import Image
 import pickle
+import os
+import boto3
 
-app = Flask(__name__)
+app = Flask(__name)
 
 @app.route('/')
 def faceRecognition():
-    recognizer = cv2.face.LBPHFaceRecognizer_create()
-    recognizer.read('trainer/trainer.yml')
-    cascadePath = "haarcascade_frontalface_default.xml"
-    faceCascade = cv2.CascadeClassifier(cascadePath)
+    # Initialize S3 client
+    s3 = boto3.client('s3')
+
+    # Specify the S3 bucket name
+    bucket_name = 'facial-login-model-bucket'  # Replace with your S3 bucket name
+
+    # Download the Haar Cascade XML file from S3
+    haarcascade_file_key = 's3://facial-login-model-bucket/haarcascade_frontalface_default.xml'
+    s3.download_file(bucket_name, haarcascade_file_key, 'haarcascade_frontalface_default.xml')
+
+    # Load the Haar Cascade XML file
+    faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+    # Download the 'names.pkl' file from S3
+    names_file_key = 's3://facial-login-model-bucket/names.pkl'
+    s3.download_file(bucket_name, names_file_key, 'names.pkl')
+
+    # Load the 'names.pkl' file
+    with open('names.pkl', 'rb') as f:
+        names = pickle.load(f)
+
+    recognizer = cv2.face_LBPHFaceRecognizer.create()
+    model_file_key = 's3://facial-login-model-bucket/trainer/trainer.yml'
+    s3.download_file(bucket_name, model_file_key, 'trainer.yml')
+    recognizer.read('trainer.yml')
 
     font = cv2.FONT_HERSHEY_SIMPLEX
 
-    # Starting realtime video capture
-    total_cameras = 2  # Total number of cameras connected
-    current_camera = 0  # Index of the current camera being used
-
-    # Starting realtime video capture
+    total_cameras = 2
+    current_camera = 0
     cam = cv2.VideoCapture(0)
-    cam.set(3, 640)  # set video width
-    cam.set(4, 480)  # set video height
-
-    # Define min window size to be recognized as a face
+    cam.set(3, 640)
+    cam.set(4, 480)
     minW = 0.1 * cam.get(3)
     minH = 0.1 * cam.get(4)
-
-    with open('names.pkl', 'rb') as f:  # Open and read the pickle file
-        names = pickle.load(f)
 
     while True:
         ret, img = cam.read()
@@ -50,7 +62,6 @@ def faceRecognition():
 
             id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
 
-            # Check if confidence is less than 100 ==> "0" is a perfect match
             if confidence < 100:
                 if id >= 0 and id < len(names):
                     id = names[id]
@@ -59,27 +70,24 @@ def faceRecognition():
                     id = "unknown"
                     confidence = "  {0}%".format(round(100 - confidence))
 
-            cv2.putText(img, str(id), (x + 5, y - 5), font, 1, (255, 255, 255), 2)  # Display the name of the individual
+            cv2.putText(img, str(id), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
             cv2.putText(img, str(confidence), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
 
         cv2.imshow('camera', img)
 
-        k = cv2.waitKey(10) & 0xff  # Press 'ESC' for exiting video
+        k = cv2.waitKey(10) & 0xff
         if k == 27:
             break
-        elif k == ord('c') or k == ord('C'):  # Switch the camera when 'C' or 'c' is pressed
+        elif k == ord('c') or k == ord('C'):
             current_camera = (current_camera + 1) % total_cameras
             cam.release()
             cam = cv2.VideoCapture(current_camera)
             cam.set(3, 640)
             cam.set(4, 480)
 
-    # Exit the video window
     print("\nExiting Recognizer.")
     cam.release()
     cv2.destroyAllWindows()
-
-faceRecognition()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
