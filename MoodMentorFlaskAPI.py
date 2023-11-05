040454351e5525ec59b16e3a0ddbc31236db0c7e
zhen_hai <113818064+zhenHai1021@users.noreply.github.com>
2023-11-05 14:53:48 +0800

    Update MoodMentorFlaskAPI.py

--- MoodMentorFlaskAPI.py
+++ MoodMentorFlaskAPI.py
@@ -4,7 +4,6 @@ import requests
 
 app = Flask(__name__)
 
-S3_PUBLIC_URL = 'https://s3.amazonaws.com/facial-login-model-bucket/hello_world.py'  # Replace with your S3 bucket URL
 
 @app.route('/')
 def hello_world():
