566f046b81ce45eac4a23a972e9a485699f6fde4
zhen_hai <113818064+zhenHai1021@users.noreply.github.com>
2023-11-05 14:45:59 +0800

    Update MoodMentorFlaskAPI.py

--- MoodMentorFlaskAPI.py
+++ MoodMentorFlaskAPI.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 import subprocess
 import requests
 
-app = Flask(__name)
+app = Flask(__name__)
 
 S3_PUBLIC_URL = 'https://s3.amazonaws.com/YOUR_BUCKET_NAME/hello_world.py'  # Replace with your S3 bucket URL
 
