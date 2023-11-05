72be33976ef1fd774731b0a81ee6306b7807b766
zhen_hai <113818064+zhenHai1021@users.noreply.github.com>
2023-11-05 14:10:58 +0800

    Update MoodMentorFlaskAPI.py

--- MoodMentorFlaskAPI.py
+++ MoodMentorFlaskAPI.py
@@ -1,37 +1,10 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-import subprocess
-import os
+from flask import Flask
 
 app = Flask(__name__)
-CORS(app)
 
-@app.route('/recognize', methods=['POST'])
-def recognize():
-    # Receive the image from the Flutter app
-    image = request.files['image']
+@app.route('/')
+def hello_world():
+    return "Hello, World!"
 
-    # Save the received image
-    image_path = 'received_image.jpg'
-    image.save(image_path)
-
-    # Run the LoginMoodMentor.py script on the received image
-    try:
-        command = f'python LoginMoodMentor.py {image_path}'
-        result = subprocess.check_output(command, shell=True)
-        result = result.decode('utf-8').strip()
-    except Exception as e:
-        result = str(e)
-
-    # Delete the temporary image file
-    if os.path.exists(image_path):
-        os.remove(image_path)
-
-    # Return the result to the Flutter app
-    response = {
-        'result': result
-    }
-    return jsonify(response)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
