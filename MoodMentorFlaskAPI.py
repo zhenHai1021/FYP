44c940fc97ccb8b213b31e85e7072de00dbccaeb
zhen_hai <113818064+zhenHai1021@users.noreply.github.com>
2023-11-07 01:10:24 +0800

    Update MoodMentorFlaskAPI.py

--- MoodMentorFlaskAPI.py
+++ MoodMentorFlaskAPI.py
@@ -34,26 +34,7 @@ def hello_world():
 
 @app.route('/recognize', methods=['POST'])
 def recognize_face():
-    try:
-        S3_PUBLIC_URL = 'https://facial-login-model-bucket.s3.amazonaws.com/LoginMoodMentor.py'  # Replace with your S3 bucket URL
-
-        # Define the full path to the Python interpreter
-        PYTHON_PATH = '/usr/bin/python3' 
-        # Fetch the 'hello_world.py' script from the public S3 URL
-        s3_script = requests.get(S3_PUBLIC_URL)
-        
-        if s3_script.status_code == 200:
-            with open('/tmp/LoginMoodMentor.py', 'wb') as f:
-                f.write(s3_script.content)
-            
-            # Use the full path to the Python interpreter
-            result = subprocess.check_output([PYTHON_PATH, '/tmp/LoginMoodMentor.py'], stderr=subprocess.STDOUT, text=True)
-            
-            return jsonify({'result': result})
-        else:
-            return jsonify({'error': f"Failed to fetch 'LoginMoodMentor.py' from S3: {s3_script.status_code}"})
-    except Exception as e:
-        return jsonify({'error': str(e)})
+    return "Heelo"
 
 
 
