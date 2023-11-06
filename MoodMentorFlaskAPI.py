a8cfcb9a627a1913f940249a5794007f96ddb33c
zhen_hai <113818064+zhenHai1021@users.noreply.github.com>
2023-11-06 13:28:09 +0800

    Update MoodMentorFlaskAPI.py

--- MoodMentorFlaskAPI.py
+++ MoodMentorFlaskAPI.py
@@ -1,31 +1,32 @@
 from flask import Flask, request, jsonify
 import subprocess
 import requests
-import os
 
 app = Flask(__name__)
 
-@app.route('/facialLogin', methods=['POST', 'GET'])
-def facialLogin():
+S3_PUBLIC_URL = 'https://facial-login-model-bucket.s3.amazonaws.com/hello_world.py'  # Replace with your S3 bucket URL
+
+# Define the full path to the Python interpreter
+PYTHON_PATH = '/usr/bin/python3'  # Modify this path if necessary
+
+@app.route('/hello_world')
+def hello_world():
     try:
-        s3_script_url = 'https://facial-login-model-bucket.s3.amazonaws.com/LoginMoodMentor.py'  # Replace with your S3 script URL
-        python_path = '/usr/bin/python3'  # Use the correct Python interpreter path
-
-        s3_script = requests.get(s3_script_url)
-
+        # Fetch the 'hello_world.py' script from the public S3 URL
+        s3_script = requests.get(S3_PUBLIC_URL)
+        
         if s3_script.status_code == 200:
-            script_path = '/tmp/LoginMoodMentor.py'
-            with open(script_path, 'wb') as f:
+            with open('/tmp/hello_world.py', 'wb') as f:
                 f.write(s3_script.content)
-
-            result = subprocess.check_output([python_path, script_path], stderr=subprocess.STDOUT, text=True)
-
+            
+            # Use the full path to the Python interpreter
+            result = subprocess.check_output([PYTHON_PATH, '/tmp/hello_world.py'], stderr=subprocess.STDOUT, text=True)
+            
             return jsonify({'result': result})
         else:
-            return jsonify({'error': f"Failed to fetch script from S3: {s3_script.status_code}"})
+            return jsonify({'error': f"Failed to fetch 'hello_world.py' from S3: {s3_script.status_code}"})
     except Exception as e:
         return jsonify({'error': str(e)})
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
