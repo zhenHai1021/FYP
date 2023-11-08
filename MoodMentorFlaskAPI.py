227c35ffe244b723ec14b776d3e7a0035454603f
zhen_hai <113818064+zhenHai1021@users.noreply.github.com>
2023-11-08 14:50:12 +0800

    Update MoodMentorFlaskAPI.py

--- MoodMentorFlaskAPI.py
+++ MoodMentorFlaskAPI.py
@@ -53,47 +53,98 @@ def download_file_from_s3(bucket_name, s3_key, local_path):
 
 @app.route('/recognize', methods=['POST'])
 def recognize_face():
-    # Define your S3 bucket and object key where the Python code is stored
-    s3_bucket = 'facial-login-model-bucket'
-    s3_object_key = 'LoginMoodMentor.py'
-
-    # Download the Python script from S3
-    local_script_path = '/tmp/LoginMoodMentor.py'
-    s3_download_error = download_file_from_s3(s3_bucket, s3_object_key, local_script_path)
-
-    if s3_download_error:
-        return {
-            'statusCode': 500,
-            'error': s3_download_error
-        }
-
-    # Execute the Python script
+    def download_file_from_s3(bucket_name, s3_key, local_path):
+    s3 = boto3.client('s3')
     try:
-        result = subprocess.run(['python3', local_script_path], capture_output=True, text=True)
-        output = result.stdout
-        error = result.stderr
+        s3.download_file(bucket_name, s3_key, local_path)
+    except botocore.exceptions.NoCredentialsError:
+        return f"S3 credentials not found for {s3_key}"
+    except botocore.exceptions.ClientError as e:
+        return f"Error downloading file from S3: {str(e)}"
+    return None
+    
 
-        if result.returncode == 0:
-            # Successfully executed
-            return {
-                'statusCode': 200,
-                'message': 'Script executed successfully',
-                'output': output,
-                'error': error
+    # Download necessary files from S3
+    s3_bucket_name = 'facial-login-model-bucket'
+    s3_trainer_yml_key = 's3://facial-login-model-bucket/trainer/trainer.yml'
+    s3_cascade_xml_key = 's3://facial-login-model-bucket/haarcascade_frontalface_default.xml'
+    s3_names_pkl_key = 's3://facial-login-model-bucket/names.pkl'
+
+    trainer_yml_error = download_file_from_s3(s3_bucket_name, s3_trainer_yml_key, 'trainer.yml')
+    cascade_xml_error = download_file_from_s3(s3_bucket_name, s3_cascade_xml_key, 'haarcascade_frontalface_default.xml')
+    names_pkl_error = download_file_from_s3(s3_bucket_name, s3_names_pkl_key, 'names.pkl')
+
+    if trainer_yml_error:
+        return [{"error": trainer_yml_error}]
+    if cascade_xml_error:
+        return [{"error": cascade_xml_error}]
+    if names_pkl_error:
+        return [{"error": names_pkl_error}]
+
+    # Load downloaded files
+    recognizer = cv2.face_LBPHFaceRecognizer.create()
+    recognizer.read('trainer.yml')
+    cascadePath = 'haarcascade_frontalface_default.xml'
+    faceCascade = cv2.CascadeClassifier(cascadePath)
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
+    cam = cv2.VideoCapture(0)  # Access the default camera (phone's camera)
+    cam.set(3, 640)  # Set video width
+    cam.set(4, 480)  # Set video height
+
+    minW = 0.1 * cam.get(3)
+    minH = 0.1 * cam.get(4)
+
+    with open('names.pkl', 'rb') as f:
+        names = pickle.load(f)
+
+    recognition_results = []  # Initialize a list to store recognition results
+
+    while True:
+        ret, img = cam.read()
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.2,
+            minNeighbors=5,
+            minSize=(int(minW), int(minH)),
+        )
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+            id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
+
+            if confidence < 100:
+                if id >= 0 and id < len(names):
+                    id = names[id]
+                    confidence = round(100 - confidence)  # Remove the '%' symbol
+                else:
+                    id = "unknown"
+                    confidence = round(100 - confidence)  # Remove the '%' symbol
+            else:
+                id = "unknown"
+                confidence = 0  # Set confidence to 0 for unrecognized faces
+
+            result = {
+                "name": id,
+                "confidence": confidence
             }
-        else:
-            return {
-                'statusCode': 500,
-                'message': 'Script execution failed',
-                'output': output,
-                'error': error
-            }
-    except Exception as e:
-        return {
-            'statusCode': 500,
-            'message': 'An error occurred during script execution',
-            'error': str(e)
-        }
+            recognition_results.append(result)
+
+            cv2.putText(img, str(id), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
+            cv2.putText(img, str(confidence), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
+
+        cv2.imshow('camera', img)
+
+        k = cv2.waitKey(10) & 0xff
+        if k == 27:
+            break
+
+    return recognition_results
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
