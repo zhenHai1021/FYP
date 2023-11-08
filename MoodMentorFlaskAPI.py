eb0e02437e667f002047a7de2aa775e321f59acf
zhen_hai <113818064+zhenHai1021@users.noreply.github.com>
2023-11-08 15:00:27 +0800

    Update MoodMentorFlaskAPI.py

--- MoodMentorFlaskAPI.py
+++ MoodMentorFlaskAPI.py
@@ -1,20 +1,23 @@
+import cv2
+import pickle
 from flask import Flask, request, jsonify
-from urllib.parse import urlparse 
 import subprocess
 import requests
-import json
 import os
 import boto3
-import subprocess
 import botocore
-import sys
-import cv2
-import numpy as np
-import pickle
 
 app = Flask(__name__)
 s3 = boto3.client('s3')
- # Modify this path if necessary
+
+def download_file_from_s3(bucket_name, s3_key, local_path):
+    try:
+        s3.download_file(bucket_name, s3_key, local_path)
+    except botocore.exceptions.NoCredentialsError:
+        return f"S3 credentials not found for {s3_key}"
+    except botocore.exceptions.ClientError as e:
+        return f"Error downloading dataset from S3: {str(e)}"
+    return None
 
 @app.route('/hello_world', methods=['POST'])
 def hello_world():
@@ -22,7 +25,8 @@ def hello_world():
         S3_PUBLIC_URL = 'https://facial-login-model-bucket.s3.amazonaws.com/hello_world.py'  # Replace with your S3 bucket URL
 
         # Define the full path to the Python interpreter
-        PYTHON_PATH = '/usr/bin/python3' 
+        PYTHON_PATH = '/usr/bin/python3'
+        
         # Fetch the 'hello_world.py' script from the public S3 URL
         s3_script = requests.get(S3_PUBLIC_URL)
         
@@ -38,18 +42,6 @@ def hello_world():
             return jsonify({'error': f"Failed to fetch 'hello_world.py' from S3: {s3_script.status_code}"})
     except Exception as e:
         return jsonify({'error': str(e)})
-
-
-
-
- def download_file_from_s3(bucket_name, s3_key, local_path):
-    try:
-        s3.download_file(bucket_name, s3_key, local_path)
-    except botocore.exceptions.NoCredentialsError:
-        return f"S3 credentials not found for {s3_key}"
-    except botocore.exceptions.ClientError as e:
-        return f"Error downloading dataset from S3: {str(e)}"
-    return None
 
 @app.route('/recognize', methods=['POST'])
 def recognize_face():
@@ -68,7 +60,7 @@ def recognize_face():
     if cascade_xml_error:
         return [{"error": cascade_xml_error}]
     if names_pkl_error:
-        return [{"error": names_pkl_error}
+        return [{"error": names_pkl_error}]
 
     # Load downloaded files
     recognizer = cv2.face_LBPHFaceRecognizer.create()
@@ -113,7 +105,7 @@ def recognize_face():
                     confidence = round(100 - confidence)  # Remove the '%' symbol
                 else:
                     id = "unknown"
-                    confidence = round 100 - confidence  # Remove the '%' symbol
+                    confidence = round(100 - confidence)  # Remove the '%' symbol
             else:
                 id = "unknown"
                 confidence = 0  # Set confidence to 0 for unrecognized faces
@@ -134,7 +126,6 @@ def recognize_face():
             break
 
     return recognition_results
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
