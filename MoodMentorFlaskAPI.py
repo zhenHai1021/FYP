e5939c2c136ce51525326122cd7aff312762601f
zhen_hai <113818064+zhenHai1021@users.noreply.github.com>
2023-11-08 15:41:38 +0800

    Update MoodMentorFlaskAPI.py

--- MoodMentorFlaskAPI.py
+++ MoodMentorFlaskAPI.py
@@ -19,30 +19,6 @@ def download_file_from_s3(bucket_name, s3_key, local_path):
         return f"Error downloading dataset from S3: {str(e)}"
     return None
 
-@app.route('/hello_world', methods=['POST'])
-def hello_world():
-    try:
-        S3_PUBLIC_URL = 'https://facial-login-model-bucket.s3.amazonaws.com/hello_world.py'  # Replace with your S3 bucket URL
-
-        # Define the full path to the Python interpreter
-        PYTHON_PATH = '/usr/bin/python3'
-        
-        # Fetch the 'hello_world.py' script from the public S3 URL
-        s3_script = requests.get(S3_PUBLIC_URL)
-        
-        if s3_script.status_code == 200:
-            with open('/tmp/hello_world.py', 'wb') as f:
-                f.write(s3_script.content)
-            
-            # Use the full path to the Python interpreter
-            result = subprocess.check_output([PYTHON_PATH, '/tmp/hello_world.py'], stderr=subprocess.STDOUT, text=True)
-            
-            return jsonify({'result': result})
-        else:
-            return jsonify({'error': f"Failed to fetch 'hello_world.py' from S3: {s3_script.status_code}"})
-    except Exception as e:
-        return jsonify({'error': str(e)})
-
 @app.route('/recognize', methods=['POST'])
 def recognize_face():
     # Download necessary files from S3
